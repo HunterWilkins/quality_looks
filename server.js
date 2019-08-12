ac1cdf4aecde0878d68f7f6af6d8ce6571c763cb
HunterWilkins <wilkins.hunter@gmail.com>
2019-08-12 18:35:46 -0500

    Added MONGODB_URI line in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/qldb"
 
 app.use(express.static("public"));
 
@@ -16,7 +17,7 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/qldb", {useNewUrlParser: true});
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
 
 // Routes
