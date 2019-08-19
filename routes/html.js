7ac4413702d8bfd762a3d127bcedb8edde40f0e4
HunterWilkins <wilkins.hunter@gmail.com>
2019-08-19 13:06:25 -0500

    Added catch for pathnames that don't match up with the database

--- routes/html.js
+++ routes/html.js
@@ -1,15 +1,30 @@
 module.exports = function(app) {
+    var db = require("../models");
+
 
     app.get("/", function(req, res) {
         res.render("home");
     });
 
-    app.get("/reviews/:id", function(req, res) {
-        res.render("reviews");
+    app.get("/reviews/:id", async(req, res) => {
+        let reviews = await db.Review.find({type: req.params.id}).exec();
+        if (reviews[0] !== undefined) {
+            res.render("reviews");
+        }
+        else {
+            res.render("catch");
+        }
+        
     });
 
-    app.get("/essay/:id", function(req, res){
-        res.render("article");
+    app.get("/essay/:id", async(req, res) => {
+        let reviews = await db.Review.find({Id: req.params.id}).exec();
+        if (reviews[0] !== undefined) {
+            res.render("article");
+        }
+        else {
+            res.render("home");
+        }
     });
 
     app.get("*", function(req, res) {
