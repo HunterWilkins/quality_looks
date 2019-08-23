ac35273b5d979237365c22c177c54388ae2c9524
HunterWilkins <wilkins.hunter@gmail.com>
2019-08-23 15:05:14 -0500

    Commented out register route

--- routes/api.js
+++ routes/api.js
@@ -108,14 +108,14 @@ module.exports = function(app) {
     });
 
     // Create New User *** ONLY TO BE USED ONCE: DELETE/COMMENT OUT IMMEDIATELY AFTER CREATING AUTH USER ***
-    app.post("/register", function (req, res) {
-        req.body.password = bcrypt.hashSync(req.body.password, 10);
-        req.body.secretId = bcrypt.hashSync(req.body.secretId, 12);
-        db.User.create(req.body)
-        .then(function(dbUser) {
-            res.json(dbUser);
-        }).catch(function(err){
-            res.json(err);
-        });
-    });
+    // app.post("/register", function (req, res) {
+    //     req.body.password = bcrypt.hashSync(req.body.password, 10);
+    //     req.body.secretId = bcrypt.hashSync(req.body.secretId, 12);
+    //     db.User.create(req.body)
+    //     .then(function(dbUser) {
+    //         res.json(dbUser);
+    //     }).catch(function(err){
+    //         res.json(err);
+    //     });
+    // });
 }
